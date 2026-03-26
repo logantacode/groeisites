50ac609bfa6340be5392c1e26072fb5b419e496d
logantacode <stefaan@stefaanoyen.be>
2026-03-26 14:56:19 +0100

    Create blog.js

---
+++ js/clients/bob-c/blog.js
@@ -0,0 +1,17 @@
+/* OPTIMALISEERT HERO BLOGARTIKELS */
+/* LAAD SITEWIDE */
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Enkel draaien bij blogartikels
+  const postHeader = document.querySelector(".post-header");
+  const postTitle = document.querySelector(".post-header h1.post-title");
+  const heroTitle = document.querySelector(".hero .text-widget-content h1");
+
+  if (!postTitle || !heroTitle) return;
+
+  // Zet de tekst van de blogtitel in de lege h1 van de hero
+  heroTitle.textContent = postTitle.textContent.trim();
+
+  // Verwijder de originele titel om dubbele h1's te verwijderen
+  postHeader.remove();
+});
